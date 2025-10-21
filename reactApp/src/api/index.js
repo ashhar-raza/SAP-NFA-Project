fd0f65da3386b8ebd30f48efed415afad4e9ceef
Ashhar Ahmad Raza <ashhar.raza@peolsolutions.com>
2025-10-21 05:55:01 +0000

    first commit

---
+++ reactApp/src/api/index.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+const baseURL = "ReactDest/";
+
+const instance = axios.create({
+  baseURL
+});
+
+export const getTableData = async (params = { $top: 100, $skip: 0 }) => {
+  const { data } = await instance.get("/NfaDetails", {
+    params
+  });
+
+  return data.d?.results || data.d || data.value;
+};
+
+export const getTableCount = async () => {
+  const { data } = await instance.get("/NfaDetails/$count");
+  return data;
+};
